1e207e4c84e46afbb7dd4cc3c19001fb0590a4ba
1320014053 <59201143+1320014053@users.noreply.github.com>
2020-07-31 22:34:45 +1200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -10,9 +10,9 @@ apt-get update
 apt-get install vim squid3 apache2-utils -y
 wget -P /etc/squid/ https://raw.githubusercontent.com/1320014053/squid/master/blacklist.list
 htpasswd -c /etc/squid/passwd sbdjm sbdjm
-htpasswd /etc/squid/passwd $name1 $name1
-htpasswd /etc/squid/passwd $name2 $name2
-htpasswd /etc/squid/passwd $name3 $name3
+htpasswd /etc/squid/passwd "$name1" "$name1"
+htpasswd /etc/squid/passwd "$name2" "$name2"
+htpasswd /etc/squid/passwd "$name3" "$name3"
 echo "$name1" >> /etc/squid/uu11.list
 echo "$name2" >> /etc/squid/uu22.list
 echo "$name3" >> /etc/squid/uu33.list
